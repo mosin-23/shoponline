ebc8dcfdac0cab77ebd82ba7e00ecef04a191f86
Abdulmosinkhanpathan <114325519+Abdulmosinkhanpathan@users.noreply.github.com>
2025-01-20 19:23:01 +0530

    k commited

--- server/controller/auth.js
+++ server/controller/auth.js
@@ -2,7 +2,7 @@ const jwt=require('jsonwebtoken')
 const User=require('../modal/userModal')
 const path=require('path')
 const fs=require('fs')
-const privateKey=process.env.PRIVATE_KEY;
+const privateKey=fs.readFileSync(path.resolve(__dirname,'../private.key'))
 const bcrypt=require('bcrypt')
 const createUser = async (req, res) => {
     try {
